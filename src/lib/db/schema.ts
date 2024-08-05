c27313b84ec4c3a7e1a7dea711a3e8574e04dcf1
anthonyprime202 <anthonyprime202@gmail.com>
2024-08-05 15:48:46 +0530

    Switched to text and added more schemas
    Switched to text datatype to varchar as they same performance and
    without connstraint they work better

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -5,10 +5,12 @@ import {
     pgSchema,
     pgTable,
     uuid,
-    varchar,
+    text,
     integer,
     primaryKey,
     boolean,
+    timestamp,
+    smallint,
 } from 'drizzle-orm/pg-core'
 
 export const genderEnum = pgEnum('genders_enum', ['MALE', 'FEMALE', 'OTHERS'])
@@ -24,8 +26,8 @@ export const accountsTable = pgTable('accounts', {
     id: uuid('id')
         .primaryKey()
         .references(() => usersTable.id, { onDelete: 'cascade' }),
-    firstName: varchar('first_name', { length: 63 }).notNull(),
-    lastName: varchar('last_name', { length: 63 }).notNull(),
+    firstName: text('first_name').notNull(),
+    lastName: text('last_name').notNull(),
     gender: genderEnum('gender').notNull(),
     dob: date('dob'),
     newslettersEnabled: boolean('newsletters_enabled').default(true),
@@ -33,25 +35,25 @@ export const accountsTable = pgTable('accounts', {
 
 export const collectionsTable = pgTable('collections', {
     id: serial('id').primaryKey(),
-    name: varchar('name', { length: 63 }).notNull(),
-    descrption: varchar('description', { length: 255 }).default(''),
+    name: text('name').notNull(),
+    descrption: text('description').default(''),
     bannerType: assetEnum('banner_type').default('IMAGE'),
-    accentColor: varchar('accent_color', { length: 7 }).default('#eeeeee'),
+    accentColor: text('accent_color').default('#eeeeee'),
 })
 
 export const productsTable = pgTable('products', {
     id: serial('id').primaryKey(),
     collectionId: integer('collection_id').references(() => collectionsTable.id),
-    name: varchar('name', { length: 63 }).notNull(),
+    name: text('name').notNull(),
     price: integer('price').notNull(),
-    washGuide: varchar('washGuide', { length: 255 }).notNull(),
-    details: varchar('details', { length: 255 }).notNull(),
+    washGuide: text('washGuide').notNull(),
+    details: text('details').notNull(),
 })
 
 export const itemsTable = pgTable('items', {
     id: serial('id').primaryKey(),
     productId: integer('product_id').references(() => productsTable.id),
-    sku: varchar('sku', { length: 255 }).notNull(),
+    sku: text('sku').notNull(),
     size: sizeEnum('size').notNull(),
     stock: integer('stock').default(0),
 })
@@ -75,11 +77,36 @@ export const cartItemsTable = pgTable(
 export const addressesTable = pgTable('adresses', {
     id: serial('id').primaryKey(),
     accountId: uuid('account_id').references(() => accountsTable.id, { onDelete: 'cascade' }),
-    title: varchar('title', { length: 63 }).notNull(),
+    title: text('title').notNull(),
     pincode: integer('pincode').notNull(),
-    state: varchar('state', { length: 63 }).notNull(),
-    city: varchar('city', { length: 63 }).notNull(),
-    lineOne: varchar('line_one', { length: 255 }).default(''),
-    lineTwo: varchar('line_two', { length: 255 }).default(''),
-    defaultAddress: boolean('default_address').default(false),
+    state: text('state').notNull(),
+    city: text('city').notNull(),
+    lineOne: text('line_one').notNull(),
+    lineTwo: text('line_two').default(''),
+    phone: text('phone').notNull(),
+    optionalPhone: text('optional_phone'),
 })
+
+export const reviewsTable = pgTable('reviews', {
+    id: serial('id').primaryKey(),
+    accountId: uuid('account_id')
+        .references(() => accountsTable.id, { onDelete: 'cascade' })
+        .notNull(),
+    productId: uuid('product_id')
+        .references(() => productsTable.id)
+        .notNull(),
+    content: text('content').notNull(),
+    rating: smallint('rating').notNull(),
+    postedAt: timestamp('postedAt').defaultNow(),
+})
+
+export const wishlistTable = pgTable(
+    'wishlist',
+    {
+        accountId: uuid('account_id').references(() => accountsTable.id),
+        collectionId: integer('collection_id').references(() => collectionsTable.id),
+    },
+    (table) => ({
+        pk: primaryKey({ columns: [table.accountId, table.collectionId] }),
+    })
+)
