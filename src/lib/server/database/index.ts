98a928e6a9fcb26171ef33c084a341f6430cdf89
anthonyprime202 <anthonyprime202@gmail.com>
2024-08-15 09:56:31 +0530

    Fixed db connection setup

--- src/lib/server/database/index.ts
+++ src/lib/server/database/index.ts
@@ -1,9 +1,10 @@
+import { DATABASE_URL } from '$env/static/private';
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
+import * as schema from './schema'
 
-const connectionString = process.env.DATABASE_URL as string
 
 // Disable prefetch as it is not supported for "Transaction" pool mode 
-const client = postgres(connectionString, { prepare: false })
-export const db = drizzle(client);
+const client = postgres(DATABASE_URL, { prepare: false })
+export const db = drizzle(client, { schema });
 
